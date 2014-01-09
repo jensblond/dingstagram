bf947a0d19f08eb47796e7e4712be9b561b0d496
Martin Szymanski <martin@elfacht.com>
2014-01-09 15:15:56 +0100

    removed spaces

--- jquery.dingstagram.js
+++ jquery.dingstagram.js
@@ -34,90 +34,90 @@
 			------------------------------------------------------*/
 			outputItems: function() {
 				$.ajax({
-	        method: "GET",
-	        dataType: "jsonp",
-	        jsonp: "callback",
-	        jsonpCallback: "jsonpcallback",
-	        cache: false,
-	        url: apiURL,
-	        success: function(data, status) {
-	        	var items = settings.items -1;
+					method: "GET",
+					dataType: "jsonp",
+					jsonp: "callback",
+					jsonpCallback: "jsonpcallback",
+					cache: false,
+					url: apiURL,
+					success: function(data, status) {
+						var items = settings.items -1;
 
 						// loop the data
-			      $.each(data.data, function(i) {
-			      	Instagram.template(data, i);
-			        return i < items; // limit the loop
-			      });
+						$.each(data.data, function(i) {
+							Instagram.template(data, i);
+							return i < items; // limit the loop
+						});
 
-			      // add classes to the target
-			      Instagram.addClasses();
-	        }
-	    	});
-	    },
+						// add classes to the target
+						Instagram.addClasses();
+					}
+				});
+			},
 
-	    /* =Create the items template
-	    ------------------------------------------------------*/
-	    template: function(data, i) {
-	    	var itemClass = settings.baseClass + '-item',
-	    			linkClass = settings.baseClass + '-link',
-	    			imgClass = settings.baseClass + '-img',
-	    			captionClass = settings.baseClass + '-caption',
-	    			figureClass = settings.baseClass + '-figure',
-	    			itemURL = data.data[i].link,
-	    			itemCaption = data.data[i].caption.text,
-	    			itemImg = '',
-	    			caption = '';
+			/* =Create the items template
+			------------------------------------------------------*/
+			template: function(data, i) {
+				var itemClass = settings.baseClass + '-item',
+						linkClass = settings.baseClass + '-link',
+						imgClass = settings.baseClass + '-img',
+						captionClass = settings.baseClass + '-caption',
+						figureClass = settings.baseClass + '-figure',
+						itemURL = data.data[i].link,
+						itemCaption = data.data[i].caption.text,
+						itemImg = '',
+						caption = '';
 
-	    	// define the image sizes
-	    	if (settings.size === 'thumbnail') {
-	    		itemImg = data.data[i].images.thumbnail.url;
-	    	} else if (settings.size === 'standard') {
-	    		itemImg = data.data[i].images.standard_resolution.url;
-	    	} else if (settings.size === 'low') {
-	    		itemImg = data.data[i].images.low_resolution.url;
-	    	}
+				// define the image sizes
+				if (settings.size === 'thumbnail') {
+					itemImg = data.data[i].images.thumbnail.url;
+				} else if (settings.size === 'standard') {
+					itemImg = data.data[i].images.standard_resolution.url;
+				} else if (settings.size === 'low') {
+					itemImg = data.data[i].images.low_resolution.url;
+				}
 
-	    	// truncate caption
-	    	if (settings.truncate > 0) {
-	    		itemCaption = Instagram.truncate(itemCaption, settings.truncate);
-	    	}
+				// truncate caption
+				if (settings.truncate > 0) {
+					itemCaption = Instagram.truncate(itemCaption, settings.truncate);
+				}
 
-	    	// append figcaption if true
-	    	if (settings.caption === true) {
-	    		caption = $(document.createElement('figcaption')).attr({'class': captionClass}).html(itemCaption)
-	    	}
+				// append figcaption if true
+				if (settings.caption === true) {
+					caption = $(document.createElement('figcaption')).attr({'class': captionClass}).html(itemCaption)
+				}
 
-	    	// creates the HTML elements
-	    	var items =
-	    			$(document.createElement('li')).attr({'class': itemClass}).append( // list item
-		    			$(document.createElement('figure')).attr({'class': figureClass}).append( // figure container
-		    				$(document.createElement('a')).attr({'href': itemURL, 'title': itemCaption, 'class': linkClass}).append( // link
-		    					$(document.createElement('img')).attr({'src': itemImg, 'alt': itemCaption, 'class': imgClass}) // image
-		    				)
-		    			).append(caption)
-	    			);
+				// creates the HTML elements
+				var items =
+						$(document.createElement('li')).attr({'class': itemClass}).append( // list item
+							$(document.createElement('figure')).attr({'class': figureClass}).append( // figure container
+								$(document.createElement('a')).attr({'href': itemURL, 'title': itemCaption, 'class': linkClass}).append( // link
+									$(document.createElement('img')).attr({'src': itemImg, 'alt': itemCaption, 'class': imgClass}) // image
+								)
+							).append(caption)
+						);
 
-	    	// append the list to the target
-	    	target.append(items);
+				// append the list to the target
+				target.append(items);
 
-	    	// add target=_blank
-	    	if (settings.blank === true) {
-	    		$('.'+linkClass).attr('target', '_blank');
-	    	}
-	    },
+				// add target=_blank
+				if (settings.blank === true) {
+					$('.'+linkClass).attr('target', '_blank');
+				}
+			},
 
-	    /* =Add classes to target
-	    ------------------------------------------------------*/
-	    addClasses: function() {
-	    	target.addClass(settings.baseClass);
-	    },
+			/* =Add classes to target
+			------------------------------------------------------*/
+			addClasses: function() {
+				target.addClass(settings.baseClass);
+			},
 
-	    /* =Truncate the caption
-	    ------------------------------------------------------*/
-	    truncate: function(content, value) {
-	    	var content = $.trim(content).substring(0, value).split(' ').slice(0, -1).join(' ') + '...';
-	    	return content;
-	    }
+			/* =Truncate the caption
+			------------------------------------------------------*/
+			truncate: function(content, value) {
+				var content = $.trim(content).substring(0, value).split(' ').slice(0, -1).join(' ') + '...';
+				return content;
+			}
 
 		}
 
